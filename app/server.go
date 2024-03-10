1522ad5af8654ac78a50768f8a500b9c0264b284
abhiraj ranjan <abhiraj.ranjan@outlook.com>
2024-03-10 21:11:26 +0530

    stage 4

--- app/server.go
+++ app/server.go
@@ -20,27 +20,35 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+		}
+
+		go handleConn(conn)
 	}
 
+}
+
+func handleConn(conn net.Conn) {
 	var b []byte
+
 	for {
 		b = make([]byte, 1024)
-		_, err = conn.Read(b)
+		_, err := conn.Read(b)
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
 			fmt.Println("error reading from client: ", err)
-			os.Exit(1)
+			continue
 		}
 
 		if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
 			fmt.Println("error writing to client: ", err)
-			os.Exit(1)
+			continue
 		}
 	}
 }
