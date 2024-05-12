77213a234ba6cc0c05903d702727801d31ab47cc
abhiraj ranjan <abhiraj.ranjan@outlook.com>
2024-05-12 06:15:11 +0530

    CLI tests (cli-test-1715474711557)

--- app/server/serverStateConfig.go
+++ app/server/serverStateConfig.go
@@ -4,25 +4,31 @@ import (
 	"sync/atomic"
 
 	"github.com/codecrafters-io/redis-starter-go/app/config"
+	"github.com/codecrafters-io/redis-starter-go/pkg/replication"
 )
 
-type serverStateConfig struct {
-	bytesProcessed atomic.Int64
+type serverStateConfig[T ~[]byte] struct {
 	*config.Config
+	repl           replication.Replication[T]
+	bytesProcessed atomic.Int64
 }
 
-func (s *serverStateConfig) ReplicationRole() config.Role {
+func (s *serverStateConfig[T]) ReplicationRole() config.Role {
 	return s.Replication.Role
 }
 
-func (s *serverStateConfig) MasterReplId() string {
+func (s *serverStateConfig[T]) MasterReplId() string {
 	return s.Replication.MasterReplId
 }
 
-func (s *serverStateConfig) MasterReplOffset() int64 {
+func (s *serverStateConfig[T]) MasterReplOffset() int64 {
 	return int64(s.Replication.MasterReplOffset)
 }
 
-func (s *serverStateConfig) BytesProcessed() int64 {
+func (s *serverStateConfig[T]) BytesProcessed() int64 {
 	return s.bytesProcessed.Load()
+}
+
+func (s *serverStateConfig[T]) ConnectedReplicas() int64 {
+	return s.repl.NumSubscriber()
 }
